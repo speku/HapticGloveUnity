44d18c035e05da8f86213930085478d2af479487
Maximilian Roeck <mroeck@stud.hs-heilbronn.de>
2016-06-26 01:14:23 +0200

    working

---
+++ HapticGloveUnity/Assets/HapticGlove/Scripts/HapticEffectSimple.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using Leap.Unity;
+using System.Linq;
+using System.Collections.Generic;
+
+public class HapticEffectSimple : MonoBehaviour {
+
+    public Hand hand;
+    public Finger finger;
+    public float zOffset = 0.017f;
+    static byte intensity = 31;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag != "LeapModel")
+        {
+            PipeClient.Vibrate(hand.ToString(), finger.ToString(), intensity);
+        }
+    }
+
+  
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag != "LeapModel")
+        {
+            PipeClient.Vibrate(hand.ToString(), finger.ToString(), 0);
+        }
+    }
+
+
+    void OnDestroy()
+    {
+        PipeClient.Close();
+    }
+
+
+    public enum Hand
+    {
+        right,
+        left
+    }
+
+    public enum Finger
+    {
+        thumb,
+        index,
+        middle,
+        ring,
+        pinky
+    }
+
+
+}
---
+++ HapticGloveUnity/Assets/HapticGlove/Scripts/PipeClient.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.IO.Pipes;
+
+public static class PipeClient  {
+
+    static NamedPipeClientStream client = new NamedPipeClientStream("HapticGlovePipe");
+    static StreamWriter writer;
+    static bool closed = false;
+
+    static PipeClient()
+    {
+        client.Connect();
+        writer = new StreamWriter(client);
+    }
+
+    public static void Vibrate(string hand, string finger, byte intensity)
+    {
+        writer.WriteLine(hand + " " + finger + " " + intensity.ToString());
+        writer.Flush();
+    }
+
+    public static void Close()
+    {
+        if (!closed)
+        {
+            client.Close();
+            closed = true;
+        }
+    }
+
+}
